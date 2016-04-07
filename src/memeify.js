092e7e38ea94a1d848917b554e934fd46e76005d
esanmiguelc <esanmiguel@gmail.com>
2016-04-06 22:18:47 -0500

    renders the top text

--- src/memeify.js
+++ src/memeify.js
@@ -1,9 +1,46 @@
 var Memeify;
 
 Memeify = (function() {
+
   function Memeify() {}
 
-  Memeify.createMeme = function(one, two) {
+  Memeify.createMeme = function(image, canvas, topText, bottomText) {
+    var context = canvas.getContext('2d');
+    var fontSize = 48;
+    context.font = fontSize + "px sans-serif";
+    var topTextWidth = context.measureText(topText).width;
+    var words = topText.split(' ');
+    var line = '';
+    var center = ((canvas.width - topTextWidth) / 2);
+    var maxWidth = (canvas.width * 0.90);
+    var y = 10;
+
+    while (topTextWidth > maxWidth) {
+      if (fontSize == 16) {
+        break;
+      }
+      fontSize -= 2;
+      context.font = fontSize + "px sans-serif";
+      topTextWidth = context.measureText(topText).width;
+    }
+
+    if (topTextWidth < maxWidth) {
+      context.fillText(topText, center, y + fontSize);
+    } else {
+      for (var i = 0; i < words.length; i++) {
+        var testLine = line + words[i] + ' ';
+        var metrics = context.measureText(testLine);
+        var testWidth = metrics.width;
+        if (testWidth > maxWidth && i > 0) {
+          context.fillText(line, center, y);
+          line = words[i] + ' ';
+          y += (fontSize + 5);
+        } else {
+          line = testLine;
+        }
+      }
+      context.fillText(line, center, y);
+    }
   };
 
   return Memeify;
--- test/memeifySpec.js
+++ test/memeifySpec.js
@@ -1,10 +1,62 @@
 describe("Memeify", function() {
-  it("adds a bottom caption to the image", function() {
-    var canvas = document.createElement('canvas');
+
+  var canvas;
+  var context;
+
+  beforeEach(function() {
+    canvas = document.createElement('canvas');
+    context = canvas.getContext('2d');
     canvas.width = 100;
     canvas.height = 100;
-    // create a fake canvas that we can spy on
-    Memeify.createMeme("src/image.jpg", canvas, "", "Some1");
-    expect().toBe();
   });
+
+  it("sets the font size and style", function() {
+    Memeify.createMeme("src/image.jpg", canvas, "", "");
+
+    expect(context.font).toBe("48px sans-serif");
+  });
+
+  it("resizes the font if text is too big", function() {
+    Memeify.createMeme("", canvas, "hellii", "");
+
+    expect(context.font).toBe("44px sans-serif")
+  });
+
+  it("only resizes up to 16px", function() {
+    Memeify.createMeme("", canvas, "super long text", "");
+
+    expect(context.font).toBe("16px sans-serif")
+  });
+
+  it("calls fill text for the top text with some padding", function() {
+    spyOn(context, 'fillText');
+    var topText = "text"
+    Memeify.createMeme("", canvas, topText, "");
+
+    expect(context.fillText)
+      .toHaveBeenCalledWith(topText, jasmine.any(Number), 58);
+  });
+
+  it("centers the text", function() {
+    spyOn(context, 'fillText');
+    var fakeMeasure = jasmine.createSpy("context");
+    spyOn(context, 'measureText').and.returnValue(fakeMeasure);
+    fakeMeasure.width = 50;
+    var topText = "text"
+
+    Memeify.createMeme("", canvas, topText, "");
+
+    expect(context.fillText)
+      .toHaveBeenCalledWith(topText, 25, jasmine.any(Number));
+  });
+
+  it("splits into multiple lines", function() {
+    spyOn(context, 'fillText');
+    Memeify.createMeme("", canvas, "super long text", "");
+
+    expect(context.fillText).toHaveBeenCalledTimes(2);
+  });
+
+  xit("sets the image", function() {});
+
 });
